ca2656a3e18fc5f66b2aeb775227ad9381072ee7
yoann foreau <yoann.foreau@ito.am>
2021-09-20 21:42:30 +0200

    Prepare option parsing

--- includes/ft_nmap.h
+++ includes/ft_nmap.h
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 15:29:05 by yforeau           #+#    #+#             */
-/*   Updated: 2021/09/20 16:30:52 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/09/20 21:37:40 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,52 @@
 # define FT_NMAP_H
 
 # include "libft.h"
+# include <limits.h>
 
 /*
 ** nmap macros
 */
 
-// stringify macro value
-# define	xstr(s)	str(s)
+# define	xstr(s)	str(s)			// stringify macro value
 # define	str(s)	#s
 
-# define	MAX_SPEEDUP	250
+# define	MAX_SPEEDUP		250
+# define	MAX_PORTS		1024	// maximum number of ports to scan
+
+// scans
+# define	S_SYN			0x01
+# define	S_NULL			0x02
+# define	S_ACK			0x04
+# define	S_FIN			0x08
+# define	S_XMAS			0x10
+# define	S_UDP			0x20
 
 # define	CONFIG_DEF	{\
-	ft_exec_name(*argv), 0\
+	ft_exec_name(*argv), 0, { 0 }, { 0 }, 0, NULL, NULL, 0\
 }
 
 /*
 ** t_nmap_config: nmap configuration
 **
 ** exec: executable name
+** speedup: number of parallel threads to use
+** ports_to_scan: boolean array representing every port given as arguments
+** ports: compressed list with the first MAX_PORTS ports of ports_to_scan
+** nb_ports: number of ports to scan in ports array
+** hosts: hosts list given by cmd argument
+** hosts_file: file containing a list of hosts
+** scans: scans to perform as an or'ed integer
 */
 typedef struct	s_nmap_config
 {
 	const char	*exec;
 	int			speedup;
+	uint8_t		ports_to_scan[USHRT_MAX + 1];
+	uint16_t	ports[MAX_PORTS + 1];
+	int			nb_ports;
+	const char	*hosts;
+	const char	*hosts_file;
+	uint8_t		scans;
 }				t_nmap_config;
 
 /*
--- src/options.c
+++ src/options.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 15:55:31 by yforeau           #+#    #+#             */
-/*   Updated: 2021/09/20 16:45:17 by yforeau          ###   ########.fr       */
+/*   Updated: 2021/09/20 21:42:03 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,33 @@
 #define	FT_NMAP_OPT	"hs:"
 
 t_opt	g_nmap_opt[] = {
+	{ "file",		1,	NULL,	'f'	},
 	{ "help",		0,	NULL,	'h'	},
+	{ "ip",			1,	NULL,	'i'	},
+	{ "ports",		1,	NULL,	'p'	},
+	{ "scan",		1,	NULL,	'S'	},
 	{ "speedup",	1,	NULL,	's'	},
 	{ NULL,			0,	NULL,	0	},
 };
 
 char	*g_nmap_help[] = {
+	"File containing a list of hosts to scan (1 per line).",
 	"Print this and exit.",
+	"Hosts to scan specified as a comma separated list of IPv4-IPv6 addresses\n"
+	"\t\tor hostnames (eg: localhost,192.168.1.0/24,2001::ffff).",
+	"Ports to scan specified as a comma separated list of individual ports or\n"
+	"\t\tranges (eg: 80,22,1024-2048). The default is 1-1024.",
+	"Scans to perform specified as a comma separated list. Possible values:\n"
+	"\t\t'SYN/NULL/FIN/XMAS/ACK/UDP' (eg: SYN,UDP). Does them all by default.",
 	"Number of parallel threads to use (def: 0, max: " xstr(MAX_SPEEDUP) ").",
 	NULL,
 };
 
 char	*g_nmap_usage[] = {
-	"[--help] [--ports number/range] [--speedup number] "
-	"[--scan type] --ip host",
-	"[--help] [--ports number/range] [--speedup number] "
-	"[--scan type] --file path",
+	"[--file path] [--help] [--ports list] [--scan list] "
+	"[--speedup number] --ip list",
+	"[--help] [--ip list] [--ports list] [--scan list] "
+	"[--speedup number] --file path",
 	NULL,
 };
 
@@ -73,6 +84,18 @@ static void	intopt(int *dest, t_optdata *optd, int min, int max)
 	}
 }
 
+void		ports_option(t_nmap_config *cfg, const char *arg)
+{
+	(void)cfg;
+	(void)arg;
+}
+
+void		scan_option(t_nmap_config *cfg, const char *arg)
+{
+	(void)cfg;
+	(void)arg;
+}
+
 void		get_options(t_nmap_config *cfg, int argc, char **argv)
 {
 	int			opt;
@@ -86,6 +109,10 @@ void		get_options(t_nmap_config *cfg, int argc, char **argv)
 	while ((opt = ft_getopt_long(argc, args, &optd)) >= 0)
 		switch (opt)
 		{
+			case 'f': cfg->hosts_file = optd.optarg;					break;
+			case 'i': cfg->hosts = optd.optarg;							break;
+			case 'p': ports_option(cfg, optd.optarg);					break;
+			case 'S': scan_option(cfg, optd.optarg);					break;
 			case 's': intopt(&cfg->speedup, &optd, 0, MAX_SPEEDUP);		break;
 			default:
 				usage(cfg->exec, opt != 'h');
