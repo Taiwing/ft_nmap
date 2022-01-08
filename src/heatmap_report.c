000b0a24c086f3c6d25b0055e7f6efd0883ff5b2
yoann foreau <yoann.foreau@ito.am>
2022-01-09 00:57:31 +0100

    feat: add a legend for the heatmap report

--- src/heatmap_report.c
+++ src/heatmap_report.c
@@ -6,16 +6,11 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 04:50:11 by yforeau           #+#    #+#             */
-/*   Updated: 2022/01/09 00:30:31 by yforeau          ###   ########.fr       */
+/*   Updated: 2022/01/09 00:56:51 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_nmap.h"
-
-//TODO:
-//- add port numbers on the sides as a reference
-//- if complete is set show an independant heatmap for each scan type
-//- add a color legend for available the different states on the map
 
 #define WIDTH_FROM_N(n)	\
 	(n > 768 ? 64 : n > 448 ? 48 : n > 160 ? 32 : n > 24 ? 16 : 8)
@@ -31,10 +26,26 @@ const char	*g_state_colors[MAX_PORT_STATUS + 1] = {
 	[ E_STATE_OPEN | E_STATE_FILTERED ] = "\e[38;5;242m",	// light grey
 };
 
+static void	print_legend(uint16_t state_counters[MAX_PORT_STATUS + 1])
+{
+	ft_printf("\n");
+	for (int state = E_STATE_OPEN; state <= MAX_PORT_STATUS; ++state)
+	{
+		if (!state_counters[state])
+			continue;
+		ft_printf("%s%s\e[0m %s ports (%hu)\n", g_state_colors[state],
+			g_port_char, g_port_status[state], state_counters[state]);
+	}
+	if (state_counters[0])
+		ft_printf("%s%s\e[0m filler (%hu)\n", g_state_colors[0],
+			g_port_char, state_counters[0]);
+}
+
 void		heatmap_report(t_host_job *host_job, t_nmap_config *cfg)
 {
 	uint8_t			state = 0, new_state;
 	const uint8_t	width = WIDTH_FROM_N(cfg->nports);
+	uint16_t		state_counters[MAX_PORT_STATUS + 1] = { 0 };
 
 	ft_printf("\n");
 	for (uint16_t id = 0; id < cfg->nports || id % width; ++id, new_state = 0)
@@ -44,6 +55,8 @@ void		heatmap_report(t_host_job *host_job, t_nmap_config *cfg)
 		ft_printf("%s%s%s", new_state != state ? g_state_colors[new_state] : "",
 			g_port_char, id % width == width - 1 ? "\n" : "");
 		state = new_state;
+		++state_counters[state];
 	}
 	ft_printf("\e[0m");
+	print_legend(state_counters);
 }
