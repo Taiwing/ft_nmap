a66036389477c83a640f1dac6d27d4f78cbcaa55
yoann foreau <yoann.foreau@ito.am>
2022-02-01 20:46:04 +0100

    fix: make sure no unfiltered packet passes through
    A filter that just drop every packet is set just after the socket is
    opened. Then we loop on recv with MSG_DONTWAIT set on until we get an
    error (which we assume to be EAGAIN or EWOULDBLOCK). This empties the
    random packets that arrived before the zero packet filter was set. Then
    everythin is blocked until we set a real filter. Clean, easy, cool ;)

--- src/network/sockets.c
+++ src/network/sockets.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 12:29:57 by yforeau           #+#    #+#             */
-/*   Updated: 2022/01/31 06:52:26 by yforeau          ###   ########.fr       */
+/*   Updated: 2022/02/01 20:38:18 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,13 +34,31 @@ static int	init_send_socket(int domain, int protocol)
 	return (sfd);
 }
 
+/*
+** This filter simply drops every packet. This is set immediately after the
+** socket is created so that we can drain the socket before setting the real
+** filter we want to set. This makes sure no invalid packet will make it through
+** to the packet parsing part of the program. Also, filter switching is done
+** atomically which means that there will be no overlap or gap between the
+** different filters set during the program's execution.
+**
+** source: https://natanyellin.com/posts/ebpf-filtering-done-right/
+*/
+struct sock_filter		g_zero_filter[] = {{ 0x06, 0, 0, 0 }};
+const struct sock_fprog	g_zero_bpf = { .len = 1, .filter = g_zero_filter };
+
 static int	init_recv_socket(int domain)
 {
-	int	sfd;
-	int	socket_protocol = domain == AF_INET ? ETH_P_IP : ETH_P_IPV6;
+	int		sfd;
+	char	drain[1];
+	int		socket_protocol = domain == AF_INET ? ETH_P_IP : ETH_P_IPV6;
 
 	if ((sfd = socket(AF_PACKET, SOCK_DGRAM, htons(socket_protocol))) < 0)
 		ft_exit(EXIT_FAILURE, "socket: %s", strerror(errno));
+	if (setsockopt(sfd, SOL_SOCKET, SO_ATTACH_FILTER,
+			&g_zero_bpf, sizeof(g_zero_bpf)) < 0)
+		ft_exit(EXIT_FAILURE, "%s: setsockopt: %s", __func__, strerror(errno));
+	while (recv(sfd, drain, sizeof(drain), MSG_DONTWAIT) >= 0);
 	return (sfd);
 }
 
