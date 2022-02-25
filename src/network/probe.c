50c7c06434c2eee567cb583fe163a5928d834afd
yoann foreau <yoann.foreau@ito.am>
2022-02-25 21:41:05 +0100

    fix: remove IP_HEADER macros

--- src/network/probe.c
+++ src/network/probe.c
@@ -6,7 +6,7 @@
 /*   By: yforeau <yforeau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 11:58:34 by yforeau           #+#    #+#             */
-/*   Updated: 2022/02/16 11:28:57 by yforeau          ###   ########.fr       */
+/*   Updated: 2022/02/25 21:40:04 by yforeau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,10 +51,10 @@ static void	init_layer4_header(t_packet *dest, uint16_t protocol,
 		.srcp = scan_job->srcp, .dstp = scan_job->dstp, .seq = 0x12344321,
 		.win = 0xfff };
 
-	if (protocol == IP_HEADER_UDP)
+	if (protocol == IPPROTO_UDP)
 		ft_init_udp_header(dest->raw_data + ipsz, dest->raw_data,
 			scan_job->srcp, scan_job->dstp);
-	else if (protocol == IP_HEADER_TCP)
+	else if (protocol == IPPROTO_TCP)
 	{
 		set_tcpflags(&tcpargs, scan_job->type);
 		ft_init_tcp_header(dest->raw_data + ipsz, &tcpargs);
@@ -67,14 +67,14 @@ void	build_probe_packet(t_packet *dest, t_scan_job *scan_job,
 	uint8_t	version = scan_job->srcip->family == AF_INET ? 4 : 6;
 	t_iph_args	ipargs = { .version = version, .dstip = scan_job->dstip,
 		.srcip = scan_job->srcip, .protocol = scan_job->type == E_UDP ?
-		IP_HEADER_UDP : IP_HEADER_TCP, .hop_limit = 255, .layer5_len = l5_len };
+		IPPROTO_UDP : IPPROTO_TCP, .hop_limit = 255, .layer5_len = l5_len };
 
 	ft_init_ip_header(dest->raw_data, &ipargs);
-	if (layer5 && l5_len && (ipargs.protocol == IP_HEADER_TCP
-		|| ipargs.protocol == IP_HEADER_UDP))
+	if (layer5 && l5_len && (ipargs.protocol == IPPROTO_TCP
+		|| ipargs.protocol == IPPROTO_UDP))
 		ft_memcpy(dest->raw_data
 			+ (version == 4 ? sizeof(struct iphdr) : sizeof(struct ipv6hdr))
-			+ (ipargs.protocol == IP_HEADER_TCP ? sizeof(struct tcphdr)
+			+ (ipargs.protocol == IPPROTO_TCP ? sizeof(struct tcphdr)
 			: sizeof(struct udphdr)), layer5, l5_len);
 	init_layer4_header(dest, ipargs.protocol, version, scan_job);
 	ft_packet_init(dest, version == 4 ? E_IH_V4 : E_IH_V6, NULL);
